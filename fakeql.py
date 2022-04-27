59c8faa437e14c601b98678b0c1522ee01649ca2
hs242424 <79582976+hs242424@users.noreply.github.com>
2022-04-27 11:56:35 -0400

    yes

--- fakeql.py
+++ fakeql.py
@@ -1,4 +1,5 @@
 import re
+from tkinter import HIDDEN
 
 def SELECT(statement):
     x = re.findall("FROM [^ ]+", statement)[0].strip('FROM ').strip("'").strip('"')
@@ -6,10 +7,18 @@ def SELECT(statement):
     if len(x) == 0:
         raise ValueError("You must enter a file path")
     try:
-        
-    print(x)
+        file = open(x)
+    except:
+        raise FileNotFoundError(f"Could not find \"{x}\"")
+
     
     
 
 def APPEND(statement):
     print(statement)
+
+def search():
+    print("Yes")
+
+def __search():
+    pass
--- test.py
+++ test.py
@@ -2,4 +2,6 @@ import fakeql as fql
 
 # SELECT('ITEM FROM "path to csv" WHERE (column number or "name")')
 
-fql.SELECT('ITEM FROM "data.csv"')
+fql.SELECT('ITEM FROM data.csv')
+
+fql.__search()
