784e85c2db7ef0c2ca3dd454ee9d27b2b8c6da81
hs242424 <79582976+hs242424@users.noreply.github.com>
2022-05-20 10:35:21 -0400

    yes

--- demo.py
+++ demo.py
@@ -2,15 +2,14 @@ import fakeql as fql
 # This is a demonstration on how the fakeql module is intended to be used
 
 
-"""x = fql.SELECT('survived, name FROM data.csv where sex = male')
-
-for i in x:
-    print(i)"""
+# x = fql.SELECT('survived, name FROM data.csv where sex = male')
+# for i in x:
+#     print(i)
 
 # fql.APPEND('data.csv, fda, dsaf,dsf wef, "This is one bit of data"')
 
 
 # fql.UPDATE('2, Survived, data.csv, "Hello There"')
 
-fql.DELETE('2, data.csv')
+# fql.DELETE('2, data.csv')
 
